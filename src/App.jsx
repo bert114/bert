e15c483181acbbac591ccfc00af10a664c58d206
bert114 <janalbertpenano14@gmail.com>
2025-03-29 22:01:34 +0800

    Fix BrowserRouter issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Project from "./components/Project";
 import NoPage from "./components/NoPage";
 import Main from "./pages/Main";
-
 function App() {
   return (
     <BrowserRouter basename="/bert">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );
